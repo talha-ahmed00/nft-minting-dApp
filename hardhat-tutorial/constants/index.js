84632ffdd2ec972641888052fb4ab94d3ba86952
talha-ahmed00 <talhasiddiqui735@gmail.com>
2022-06-13 21:05:46 +0500

    commit to main

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x92B338bb5c052952f266Ba4634dE06f6A92E547A";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
